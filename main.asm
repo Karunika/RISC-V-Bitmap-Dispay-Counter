490b0cca61a37219211990b91122006c86ee09bd
Karunika <karunikatrip@gmail.com>
2023-12-22 07:17:10 +0100

    fix: removed trailing zeros

--- main.asm
+++ main.asm
@@ -66,6 +66,46 @@ draw_line:
 
 .end_macro
 
+.macro erase(%position)
+
+    la a1, buffer_frame
+
+    lw a4, width
+    lw a5, height
+
+    # buffer_frame += width * position * word_size
+    lw t3, width
+    mv t4, %position
+    mul t3, t3, t4
+    li t4, 4
+    mul t3, t3, t4
+    add a1, a1, t3
+
+draw_line:
+    li t3, 0
+	sw t3, 0(a1)   # store the content of a3 (color) in the address pointed by a1
+
+    # advance to next frame buffer pixel position and _number reference
+    addi a1, a1, 4
+
+	addi a4, a4, -1
+    bnez a4, draw_line
+
+    # buffer_frame += (canvas_width - width) * word_size
+    lw t3, width
+    lw t4, canvas_width
+    sub t3, t4, t3
+    li t4, 4
+    mul t3, t3, t4
+    add a1, a1, t3
+
+    lw t3, width
+    add a4, a4, t3 # replenish width
+    addi a5, a5, -1  # decrease height counter
+    bnez a5, draw_line
+
+.end_macro
+
 .text
 
 main:
@@ -128,9 +168,20 @@ loop_digits:
 
     addi t6, t6, -1
 
+    beqz t2, erase_zero
+
+    li a1, 10
+    div t1, t1, a1
+    div t2, t2, a1
+
     mv t4, a3
     jal map_t4_to_label
     draw_num t4, t6
+
+    j loop_digits
+
+erase_zero:
+    erase t6
 
     li a1, 10
     div t1, t1, a1
